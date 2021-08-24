08d085271471b50fb6262b8f220ddeb7aa6b27c7
Sallys Carlos <sallys.carlos93@gmail.com>
2021-08-24 18:29:56 -0300

    Update controller

--- controller/usuario.py
+++ controller/usuario.py
@@ -1,4 +1,5 @@
-from flask import render_template, request, Blueprint
+from flask import render_template, request, Blueprint, url_for
+from werkzeug.utils import redirect
 from config import db
 from model.usuario import Usuario
 
@@ -31,13 +32,19 @@ def consultarUsuarios():
 def abrirCadastroUsuario():
     return render_template('cadastrarUsuario.html')
 
-@usuario_blueprint.route('/usuario/edit', methods=['POST'])
-def viewFormEdit():
-    nome = request.form.get('nome')
-    email = request.form.get('email')
-    usuario = Usuario.query.filter(Usuario.email == email)
-    return render_template('editarUsuario.html', usuario=usuario.id)
+@usuario_blueprint.route('/usuario/edit/<int:index>', methods=['GET', 'POST'])
+def abrirEditarUsuario(index):
+    if request.method == 'POST':
+        nome = request.form.get('nome')
+        email = request.form.get('email')
+        usuario = Usuario.query.filter_by(Usuario.id == index).first()
+        usuario.nome = nome
+        usuario.email = email
+        db.session.commit()
+        return redirect(url_for('usuario_blueprint.consultarUsuarios'))
+    return render_template('editarUsuario.html', index=index)
 
-@usuario_blueprint.route('/usuario/delete')
-def deleteUser():
-    db.session.delete()
+@usuario_blueprint.route('/delete/<int:index>', methods=['POST', 'GET'])
+def delete(index):
+    Usuario.query.filter(Usuario.id == index).delete()
+    return redirect(url_for('usuario_blueprint.consultarUsuarios'))
