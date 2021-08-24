8014ac8053ee68f3482eeb45f7698defbb6f7193
Sallys Carlos <53477221+scsSilva@users.noreply.github.com>
2021-08-24 20:50:29 -0300

    Update app.py

--- app.py
+++ app.py
@@ -25,11 +25,4 @@ def helloWorld():
 def login():
     return render_template('index.html')
 
-# @app.route('/index')
-# def index():
-#     nome = 'Flávio'
-#     mostrarVideos = True
-#     lista = ['https://www.youtube.com/embed/vU3RHRELdCE', 'https://www.youtube.com/embed/uUUYv_T1dEs', 'https://www.youtube.com/embed/n3tMEOw9KGY']
-#     return render_template('hello-world.html', nome=nome, lista=lista, mostrarVideos=mostrarVideos)
-
-app.run(host="0.0.0.0", port=5000)
+# app.run(host="0.0.0.0", port=5000)
